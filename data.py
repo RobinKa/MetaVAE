17e5357035b81c1e5e2f97054abba2e837297a7c
Robin Kahlow <robinkahlow@web.de>
2018-12-09 17:37:16 +0000

    check for image extensions when loading data

--- data.py
+++ data.py
@@ -9,15 +9,16 @@ def _load_image(params):
     path, size = params
     return resize(imread(path), size).astype(np.float32)
 
-def load_images_by_directories(root_path, min_samples, target_size):
+def load_images_by_directories(root_path, min_samples, target_size, supported_extensions=[".jpg", ".jpeg", ".png", ".gif", ".bmp"]):
     """Returns a list of images grouped by label. Takes the image directory as its label.
     Works with nested directories."""
     images_by_label = []
 
     with Pool() as pool:
         for root, _, filenames in tqdm(os.walk(root_path)):
-            if len(filenames) >= min_samples:
-                images = pool.map(_load_image, [(os.path.join(root, path), target_size) for path in filenames])
+            matching_files = [name for name in filenames if os.path.splitext(name)[1].lower() in supported_extensions]
+            if len(matching_files) >= min_samples:
+                images = pool.map(_load_image, [(os.path.join(root, path), target_size) for path in matching_files])
                 images_by_label.append(np.array(images))
 
     return images_by_label
